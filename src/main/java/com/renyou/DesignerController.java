d7046af95947f3d38e276d1f5b51a7258a75ac15
bsaahil <35931207+bsaahil@users.noreply.github.com>
2018-02-03 14:25:34 +0530

    Adding Designer crud

--- src/main/java/com/renyou/DesignerController.java
+++ src/main/java/com/renyou/DesignerController.java
@@ -1,8 +1,43 @@
 package com.renyou;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.renyou.db.Designer;
+import com.renyou.db.DesignerRepository;
+
 
 @Controller
 public class DesignerController {
+	
+	@Autowired
+	private DesignerRepository designerRepository;
+	
+	
+	@RequestMapping("/listDesigners")
+	public String designerList(Model model) {
+		model.addAttribute("designers", designerRepository.findAll());
+		return "list-designer";
+	}
 
+	@RequestMapping(value = "/addDesigner", method = RequestMethod.GET)
+	public String designersAdd(@RequestParam(value = "id", required = false) Integer id, Model model) {
+		if (id != null) {
+			Designer space = designerRepository.findOne(id);
+			model.addAttribute("designer", space);
+		}
+		return "edit-designer";
+	}
+
+	@RequestMapping(value = "/saveDesigner", method = RequestMethod.POST)
+	public String DesignerSave(Designer designer, Model model) {
+		designerRepository.save(designer);
+		model.addAttribute("designer", designer);
+		model.addAttribute("message", "Designer " + designer.getName() + " saved successfully");
+		return "edit-designer";
+	}
 }
---
+++ src/main/java/com/renyou/db/Designer.java
@@ -0,0 +1,66 @@
+package com.renyou.db;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Designer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@NotNull
+	@Column(unique = true)
+	private String name;
+
+	private String description;
+
+	private String address;
+
+	private String city;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
